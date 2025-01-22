42b17ce90013e9c23cf5c789c803fc1b833419a8
Andreas Klebe <a.klebe.work@gmail.com>
2025-01-22 19:25:11 +0100

    - bash improvements - new ui bash option "r" for raw update

--- bash-scripts/scripts/git/checkout-one-branch-for-all-modules.sh
+++ bash-scripts/scripts/git/checkout-one-branch-for-all-modules.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#su - hiroki
-
 # =============================================
 # Checking location is the script folder
 # =============================================
@@ -12,11 +10,17 @@ if [[ $BASEDIR != *"git" ]]; then
   exit
 fi
 
+# Argument validation check
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <branchToCheckout>"
+    exit 1
+fi
+
 # =============================================
 # Preparing ...
 # =============================================
-PROJECTDIR="../.."
-branchToCheckout="master"
+PROJECTDIR="$BASEDIR/../.."
+branchToCheckout="$1"
 
 # =============================================
 # Read default values ...
@@ -38,26 +42,65 @@ branchToCheckout="master"
 # calculate variables depends on config
 # =============================================
 fullModulePath="$destination_mercy_root_path/$mercyModuleDirectory"
+fullThemePath="$destination_mercy_root_path/$mercyThemeDirectory"
 
 # =============================================
 # Confirmation
 # =============================================
-f_confirmation_mercy_root_settings "Checkout branch '$branchToCheckout' for all GIT repositories\nin '$fullModulePath'?"
+f_confirmation_mercy_root_settings "Checkout branch '$branchToCheckout' for all GIT repositories\nin '$fullModulePath' and '$fullThemePath'?"
 [ ! $? -eq 0 ] && { exit 1; } # return not 0 = error and exit
 
 # =============================================
 #
 # =============================================
+function gitStuff() {
+
+      # check its a git repo
+      if [ -f "$1/.git/config" ]; then
+
+        cd "$1" || exit
+        echo "Try branch '$2' in '$1' ..."
+
+        if output=$(git status --porcelain) && [ -z "$output" ]; then
+          echo "OK!"
+        else
+          echo "Skipped! No clean git repository."
+        fi
+
+        git fetch origin "$2"
+        git checkout "$2"
+        echo "" # new line
+
+      else
+
+        echo "No git found in '$1'."
+
+      fi
+
+}
+
+# =============================================
+# App itself
+# =============================================
+if [ -d "$destination_mercy_root_path" ]; then
+  gitStuff "$destination_mercy_root_path" "$branchToCheckout"
+fi
+
+# =============================================
+# Modules
+# =============================================
 for d in $fullModulePath/*; do
   if [ -d "$d" ]; then
-    # check its a git repo
-    if [ -f "$d/.git/config" ]; then
-      echo "$d : git fetch and checkout ..."
-      cd "$d" || exit
-#      git fetch origin "$branchToCheckout"
-#      git checkout "$branchToCheckout"
-      echo "" # new line
-    fi
+    gitStuff "$d" "$branchToCheckout"
+  fi
+done
+
+# =============================================
+# Themes
+# =============================================
+for d in $fullThemePath/*; do
+  if [ -d "$d" ]; then
+    gitStuff "$d" "$branchToCheckout"
   fi
 done
 
