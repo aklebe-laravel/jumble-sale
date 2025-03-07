43d37d1e0567456a99f6a2a6306aaf92acb7f001
Andreas Klebe <a.klebe.work@gmail.com>
2025-03-07 18:47:02 +0100

    - use now arrayMergeRecursiveDistinct() in forms makeObjectModelInstance() - added user extra_attribute currency - fixed form defaults for extra_attributes - Dusk test

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -12,12 +12,6 @@ class LoginTest extends DuskTestCase
      */
     public function testLogin(): void
     {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/')->assertSee(__('Login'));
-            $browser->type('email', data_get(static::users, 'admin2.email'))->type('password', data_get(static::users, 'admin2.password'))->press(__('Login'))->waitForRoute('home', seconds: self::maxWaitInSeconds)
-                // ->assertRouteIs('home');
-                ->assertSee(__('Cart'));
-            $browser->screenshot('logged_in');
-        });
+        $this->loginDuskUser('trader1');
     }
 }
